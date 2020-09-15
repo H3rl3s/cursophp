2b58f30d8bfdd5fdf61ede68d040580f9b70ab2b
H3rl3s <herlesp@gmail.com>
2020-09-15 01:46:40 -0300

    alteração no listaproduto.php

--- listaprodutos.php
+++ listaprodutos.php
@@ -3,21 +3,19 @@
 <head>
 	<meta charset="utf-8">
 	<title>Lista de Produtos</title>
-	
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    
-
+	  <link rel="stylesheet" href="css/bootstrap.css">
 	</head>
 
 
 <body>
+
 
 <div class="container" style="margin-top: 40px">
 
   <div style="text-align: right;">
     <a href="index.php" role="button" class="btn btn-sm btn-primary">Voltar</a>
   </div>
-  
+
   <h3>Lista de Produtos</h3>
 
   <table class="table">
@@ -57,7 +55,7 @@
       <td> <?php echo $fornecedor ?></td>
       <td>
         <a class="btn btn-warning btn-sm" style="color:#fff" href="editarproduto.php?id=<?php echo $id_estoque ?>" role="button"><i class="far fa-edit"></i>&nbsp;Editar</a>
-        <a class="btn btn-danger btn-sm" style="color:#fff" href="excluir.php?id=<?php echo $id_estoque ?>" role="button"><i class="fas fa-trash-alt"></i></i></i>&nbsp;Excluir</a>
+        <a class="btn btn-danger btn-sm" style="color:#fff" href="excluir.php?id=<?php echo $id_estoque ?>" role="button"><i class="fas fa-trash-alt"></i>&nbsp;Excluir</a>
       </td>
     </tr>
       <?php 
@@ -71,9 +69,8 @@
 
 
 </div>
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/ff2489b726.js" crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
 
 </body>
 </html>
